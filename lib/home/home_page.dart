9877b57a98f41758176691e9f6e92ed90163cf34
Wellington Prado <well.cp@hotmail.com>
2021-05-18 08:47:39 -0300

    Desafio 02 initial

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -9,47 +9,61 @@ class _HomePageState extends State<HomePage> {
   var notes = <String>[];
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("NOTES"),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            for (var i = 0; i < notes.length; i++)
-              Card(
-                child: ListTile(
-                  title: Text(notes[i]),
-                  onTap: () async {
-                    var response = await Navigator.pushNamed(
-                        context, "/create-note",
-                        arguments: notes[i]);
-                    if (response != null) {
-                      var description = response as String;
-                      if (response.isEmpty) {
-                        notes.removeAt(i);
-                      } else {
-                        notes[i] = description;
+        child: Container(
+          child: Column(
+            children: [
+              for (var i = 0; i < notes.length; i++)
+                Card(
+                  child: ListTile(
+                    title: Text(notes[i]),
+                    onTap: () async {
+                      var response = await Navigator.pushNamed(
+                          context, "/create-note",
+                          arguments: notes[i]);
+                      if (response != null) {
+                        var description = response as String;
+                        if (response.isEmpty) {
+                          notes.removeAt(i);
+                        } else {
+                          notes[i] = description;
+                        }
+                        setState(() {});
                       }
-                      setState(() {});
-                    }
-                  },
+                    },
+                  ),
+                ),
+              Center(
+                child: Container(
+                  child: SizedBox(
+                    height: 50,
+                    width: 200,
+                    child: Expanded(
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          var description = await Navigator.pushNamed(
+                              context, "/create-note");
+                          setState(() {
+                            if (description != null) {
+                              notes.add(description as String);
+                            }
+                          });
+                        },
+                        child: Text("Adicionar"),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-          ],
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async {
-          var description = await Navigator.pushNamed(context, "/create-note");
-          setState(() {
-            if (description != null) {
-              notes.add(description as String);
-            }
-          });
-        },
       ),
     );
   }
